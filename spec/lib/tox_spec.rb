08081c81d699407765b31c08256eb2847bee634e
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-18 13:45:23 +0000

    Add specs for Tox#savedata

--- spec/lib/tox_spec.rb
+++ spec/lib/tox_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Tox do
       expect(described_class::VERSION).to match(/\A(\d+)\.(\d+)\.(\d+)\z/)
     end
   end
+
+  describe '#savedata' do
+    it 'can be set via options' do
+      tox1_options = Tox::Options.new
+      tox1 = Tox.new tox1_options
+
+      savedata = tox1.savedata
+
+      tox2_options = Tox::Options.new
+      tox2_options.savedata = savedata
+      tox2 = Tox.new tox2_options
+
+      expect(tox2.savedata).to eq savedata
+    end
+  end
 end
