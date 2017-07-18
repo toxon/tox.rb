cb1ea5a080b48a1f7187aeb2f1c7ca75037aaf4c
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-18 11:52:51 +0000

    Add method Tox#savedata

--- ext/tox/extconf.rb
+++ ext/tox/extconf.rb
@@ -14,5 +14,7 @@ have_func 'memset' and
 
 have_library LIBTOXCORE, 'tox_new' and
 have_library LIBTOXCORE, 'tox_options_default' and
+have_library LIBTOXCORE, 'tox_get_savedata_size' and
+have_library LIBTOXCORE, 'tox_get_savedata' and
 
 create_makefile "#{NAME}/#{NAME}" or exit 1
