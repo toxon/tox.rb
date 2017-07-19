43b04e57ff8209b09d152d590a844266de59c78d
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-19 12:15:12 +0000

    Expect macros

--- ext/tox/extconf.rb
+++ ext/tox/extconf.rb
@@ -12,6 +12,9 @@ have_func 'free' and
 have_func 'memset' and
 have_func 'sprintf' and
 
+have_macro 'TOX_VERSION_IS_API_COMPATIBLE', 'tox/tox.h' and
+have_macro 'TOX_VERSION_IS_ABI_COMPATIBLE', 'tox/tox.h' and
+
 have_const 'TOX_SAVEDATA_TYPE_TOX_SAVE', 'tox/tox.h' and
 have_const 'TOX_ERR_NEW_OK',             'tox/tox.h' and
 have_const 'TOX_ADDRESS_SIZE',           'tox/tox.h' and
