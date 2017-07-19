71b329a0529e0bda6346f66e2ec2b7e1a2a4c563
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-19 19:38:20 +0000

    Add specs for Tox::Client#initialize

--- spec/lib/tox/client_spec.rb
+++ spec/lib/tox/client_spec.rb
@@ -5,6 +5,20 @@ require 'tox'
 RSpec.describe Tox::Client do
   subject { described_class.new }
 
+  describe '#initialize' do
+    context 'when options is nil' do
+      specify do
+        expect { described_class.new nil }.to raise_error TypeError, "expected options to be a #{Tox::Options}"
+      end
+    end
+
+    context 'when options have invalid type' do
+      specify do
+        expect { described_class.new 123 }.to raise_error TypeError, "expected options to be a #{Tox::Options}"
+      end
+    end
+  end
+
   describe '#savedata' do
     it 'returns string by default' do
       expect(subject.savedata).to be_a String
