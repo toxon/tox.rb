194ea1b8a685271a94a3451c42ffe822d30515e5
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2017-07-25 21:10:01 +0000

    Align echo bot log format

--- examples/echo_bot.rb
+++ examples/echo_bot.rb
@@ -49,10 +49,10 @@ end
 
 tox_client.on_friend_message do |friend, text|
   puts 'Message from friend'
-  puts "Number: #{friend.number}"
-  puts "Name: #{friend.name}"
-  puts "Public key: #{friend.public_key.to_hex}"
-  puts "Status: #{friend.status}"
+  puts "Number:         #{friend.number}"
+  puts "Name:           #{friend.name}"
+  puts "Public key:     #{friend.public_key.to_hex}"
+  puts "Status:         #{friend.status}"
   puts "Status message: #{friend.status_message}"
   puts text.inspect
   puts
