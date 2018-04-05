e74e350d09cd142923d8d15747b9a59c9f6d594c
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-05 22:23:02 +0000

    Add multiple test nodes

--- spec/integration/two_clients_spec.rb
+++ spec/integration/two_clients_spec.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Two clients' do
-  let! :node_pid do
-    Process.spawn(
-      node_executable,
-      '--foreground',
-      '--log-backend', 'stdout',
-      '--config', node_config,
-    )
+  let! :node_pids do
+    node_configs.map do |node_config|
+      Process.spawn(
+        node_executable,
+        '--foreground',
+        '--log-backend',
+        'stdout',
+        '--config',
+        node_config,
+      )
+    end
   end
 
   after do
-    Process.kill :SIGINT, node_pid
+    node_pids.each do |node_pid|
+      Process.kill :SIGINT, node_pid
+    end
   end
-
-  let(:node_config) { File.expand_path 'config/node.conf' }
 
   let :node_executable do
     if File.executable? node_vendored
@@ -29,16 +33,38 @@ RSpec.describe 'Two clients' do
   let(:node_vendored) { File.expand_path 'vendor/libtoxcore/tox-bootstrapd' }
   let(:node_local) { '/usr/local/src/c-toxcore/_build/tox-bootstrapd' }
 
+  let :node_configs do
+    [
+      File.expand_path('config/node1_conf'),
+      File.expand_path('config/node2_conf'),
+      File.expand_path('config/node3_conf'),
+    ]
+  end
+
+  let :nodes do
+    [
+      Tox::Node.new(
+        ipv4: '127.0.0.1',
+        port: 10_100,
+        public_key: 'A8020928C0B6AE8665A532C1084D1344' \
+                    'CCC96724670122A1CB879E36F85A7D60',
+      ),
+      Tox::Node.new(
+        ipv4: '127.0.0.1',
+        port: 10_200,
+        public_key: '88A8100DEEDE5223603231768C64BDF0' \
+                    '27667C0ADC58ED006DED26D1881E1122',
+      ),
+      Tox::Node.new(
+        ipv4: '127.0.0.1',
+        port: 10_300,
+        public_key: '89D8F36C2201371B9B3CD3EE7EC8E834' \
+                    '319FEE8014E02F949BDD2DE7E5E5167D',
+      ),
+    ]
+  end
+
   specify do
-    node_public_key =
-      'A8020928C0B6AE8665A532C1084D1344CCC96724670122A1CB879E36F85A7D60'
-
-    node = Tox::Node.new(
-      ipv4: '127.0.0.1',
-      port: 33_445,
-      public_key: node_public_key,
-    )
-
     options = Tox::Options.new
     # FIXME: use bootstrap node instead of local discovery
     options.local_discovery_enabled = true
@@ -55,8 +81,10 @@ RSpec.describe 'Two clients' do
     sleep 0.1 until send_client.friends.last.exist?
     sleep 0.1 until recv_client.friends.last.exist?
 
-    expect(send_client.bootstrap(node)).to eq true
-    expect(recv_client.bootstrap(node)).to eq true
+    nodes.each do |node|
+      expect(send_client.bootstrap(node)).to eq true
+      expect(recv_client.bootstrap(node)).to eq true
+    end
 
     send_client.on_iteration do
       send_queue.size.times do
