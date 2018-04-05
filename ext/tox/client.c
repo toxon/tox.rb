201b4eb6244423c697d819ae6bd61b7791ecd1b3
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-05 09:39:05 +0000

    Fix error

--- ext/tox/client.c
+++ ext/tox/client.c
@@ -208,7 +208,7 @@ VALUE mTox_cClient_bootstrap(const VALUE self, const VALUE node)
   tox_bootstrap(self_cdata->tox,
                 RSTRING_PTR(rb_funcall(node, rb_intern("resolv_ipv4"), 0)),
                 NUM2INT(rb_funcall(node, rb_intern("port"), 0)),
-                RSTRING_PTR(rb_funcall(node, rb_intern("public_key"), 0)),
+                RSTRING_PTR(rb_funcall(rb_funcall(node, rb_intern("public_key"), 0), rb_intern("value"), 0)),
                 &error);
 
   switch (error) {
