ec5bcde5fd0e1a78e0ccc0eb4279100408b9a8ec
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-09 20:19:33 +0000

    Improve class Support::Process

--- spec/support/fake_bootstrap_node/process.rb
+++ spec/support/fake_bootstrap_node/process.rb
@@ -12,25 +12,61 @@ module Support
         ROOT_DIR,
       ).freeze
 
-      attr_reader :config_file_path
+      attr_reader :config_file_path, :stdout_lines, :stderr_lines
 
       def initialize(config_file_path)
         self.config_file_path = config_file_path
+        start_process
+        @stdout_lines = [].freeze
+        @stderr_lines = [].freeze
+        stdout_poll_thread
+        stderr_poll_thread
+      end
 
-        _stdin, @stdout, @wait_thr = Open3.popen2(
-          DAEMON_FILE_PATH,
-          '--foreground',
-          '--log-backend',
-          'stdout',
-          '--config',
-          self.config_file_path,
-        )
+      def close
+        ::Process.kill :SIGKILL, @wait_thr.pid
+        nil
       end
 
     private
 
       def config_file_path=(value)
         @config_file_path = value.frozen? ? value : value.dup.freeze
+      end
+
+      def start_process
+        _stdin, @stdout, @stderr, @wait_thr = Open3.popen3(
+          DAEMON_FILE_PATH,
+          '--foreground',
+          '--log-backend',
+          'stdout',
+          '--config',
+          config_file_path,
+        )
+      end
+
+      def stdout_poll_thread
+        @stdout_poll_thread ||= Thread.start do
+          until @stdout.eof?
+            begin
+              @stdout_lines = [*@stdout_lines, @stdout.readline].freeze
+            rescue EOFError
+              IO.select [@stdout]
+            end
+          end
+        end
+      end
+
+      def stderr_poll_thread
+        @stderr_poll_thread ||= Thread.start do
+          until @stderr.eof?
+            begin
+              @stderr_lines = [*@stderr_lines, @stderr.readline].freeze
+            rescue EOFError
+              IO.select [@stderr]
+            end
+          end
+        end
       end
     end
   end
