ef12a9edde83fc54e59ecdee6f493b030ffc354d
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-15 13:16:36 +0000

    Remove fake proxy

--- spec/network_helper.rb
+++ spec/network_helper.rb
@@ -9,23 +9,6 @@ require 'spec_helper'
 
 require 'support/fake_bootstrap_network'
 
-ROOT_DIR = File.expand_path('..', __dir__).freeze
-
-HTTP_PROXY_EXECUTABLE = File.join(ROOT_DIR, 'bin/proxy').freeze
-
-HTTP_PROXY_PORT = rand 1024..65_535
-
 RSpec.configure do |config|
   config.include Support::FakeBootstrapNetwork
-
-  config.before :suite do
-    $http_proxy_pid = Process.spawn(
-      HTTP_PROXY_EXECUTABLE,
-      HTTP_PROXY_PORT.to_s,
-    )
-  end
-
-  config.after :suite do
-    Process.kill :SIGINT, $http_proxy_pid
-  end
 end
