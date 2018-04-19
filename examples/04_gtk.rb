fb5e22db2af6867beed45d265845e5f9d8ff09e6
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-19 03:48:01 +0000

    Acceppt friendship requests

--- examples/04_gtk.rb
+++ examples/04_gtk.rb
@@ -3,9 +3,31 @@
 
 require 'bundler/setup'
 
+require 'tox'
 require 'gtk3'
 
+NAME = 'GTK Client'
+
 GLADE_FILE = File.expand_path('ui.glade', __dir__).freeze
+
+Thread.abort_on_exception = true
+
+friends_list_store = Gtk::ListStore.new Integer, String
+
+tox_client = Tox::Client.new
+
+tox_client.name = NAME
+tox_client.status = Tox::UserStatus::NONE
+
+puts "Address: #{tox_client.address}"
+
+puts 'Connecting to the nodes from official list...'
+tox_client.bootstrap_official
+
+tox_client.on_friend_request do |public_key, _text|
+  friend = tox_client.friend_add_norequest public_key
+  friends_list_store.append.set_values 0 => friend.number
+end
 
 gtk_builder = Gtk::Builder.new
 
@@ -17,9 +39,8 @@ end
 
 def on_friends_tree_selection_changed_cb(selection)
   selected = selection.selected
-  return if selected.nil?
-  friend_number = selected[0]
-  p friend_number
+  return $current_friend_number = nil if selected.nil?
+  $current_friend_number = selected[0]
 end
 
 gtk_builder.connect_signals do |handler|
@@ -29,18 +50,18 @@ end
 gtk_builder['main_window'].show_all
 
 gtk_builder['friends_tree_view'].append_column(
-  Gtk::TreeViewColumn.new('Name', Gtk::CellRendererText.new, text: 1),
+  Gtk::TreeViewColumn.new('#', Gtk::CellRendererText.new, text: 0),
 )
-
-friends_list_store = Gtk::ListStore.new Integer, String
 
 gtk_builder['friends_tree_view'].model = friends_list_store
 
-10.times do
-  friends_list_store.append.set_values 0 => 0, 1 => 'Braiden Vasco'
-  friends_list_store.append.set_values 0 => 1, 1 => 'Satoshi Nakamoto'
-  friends_list_store.append.set_values 0 => 2, 1 => 'Elon Musk'
-  friends_list_store.append.set_values 0 => 3, 1 => 'a' * 50
+puts 'Running. Send me friend request, I\'ll accept it immediately.'
+
+Thread.start do
+  loop do
+    sleep tox_client.iteration_interval
+    tox_client.iterate
+  end
 end
 
 Gtk.main
