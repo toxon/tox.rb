f7b153d94b5f8d8ed68de9144f9ecd371e6b5e33
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-29 15:59:29 +0000

    Fix code style

--- config/simplecov.rb
+++ config/simplecov.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter,
-])
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter,
+  ],
+)
 
 SimpleCov.start do
   add_filter '/spec/'
