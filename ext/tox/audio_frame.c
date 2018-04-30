7a295ddb7ba2d6621fd6c7cf69ca9517678f6b72
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-30 15:56:20 +0000

    Add setter methods to Tox::AudioFrame

--- ext/tox/audio_frame.c
+++ ext/tox/audio_frame.c
@@ -7,8 +7,13 @@ static void  mTox_cAudioFrame_free(mTox_cAudioFrame_CDATA *free_cdata);
 // Public methods
 
 static VALUE mTox_cAudioFrame_sample_count(VALUE self);
+static VALUE mTox_cAudioFrame_sample_count_ASSIGN(VALUE self, VALUE sample_count);
+
 static VALUE mTox_cAudioFrame_channels(VALUE self);
+static VALUE mTox_cAudioFrame_channels_ASSIGN(VALUE self, VALUE channels);
+
 static VALUE mTox_cAudioFrame_sampling_rate(VALUE self);
+static VALUE mTox_cAudioFrame_sampling_rate_ASSIGN(VALUE self, VALUE sampling_rate);
 
 /*************************************************************
  * Initialization
@@ -21,12 +26,14 @@ void mTox_cAudioFrame_INIT()
 
   // Public methods
 
-  rb_define_method(mTox_cAudioFrame, "sample_count",
-                   mTox_cAudioFrame_sample_count, 0);
-  rb_define_method(mTox_cAudioFrame, "channels",
-                   mTox_cAudioFrame_channels, 0);
-  rb_define_method(mTox_cAudioFrame, "sampling_rate",
-                   mTox_cAudioFrame_sampling_rate, 0);
+  rb_define_method(mTox_cAudioFrame, "sample_count",  mTox_cAudioFrame_sample_count,        0);
+  rb_define_method(mTox_cAudioFrame, "sample_count=", mTox_cAudioFrame_sample_count_ASSIGN, 1);
+
+  rb_define_method(mTox_cAudioFrame, "channels",  mTox_cAudioFrame_channels,        0);
+  rb_define_method(mTox_cAudioFrame, "channels=", mTox_cAudioFrame_channels_ASSIGN, 1);
+
+  rb_define_method(mTox_cAudioFrame, "sampling_rate",  mTox_cAudioFrame_sampling_rate,        0);
+  rb_define_method(mTox_cAudioFrame, "sampling_rate=", mTox_cAudioFrame_sampling_rate_ASSIGN, 1);
 }
 
 /*************************************************************
@@ -36,6 +43,8 @@ void mTox_cAudioFrame_INIT()
 VALUE mTox_cAudioFrame_alloc(const VALUE klass)
 {
   mTox_cAudioFrame_CDATA *alloc_cdata = ALLOC(mTox_cAudioFrame_CDATA);
+
+  memset(alloc_cdata, 0, sizeof(mTox_cAudioFrame_CDATA));
 
   return Data_Wrap_Struct(klass, NULL, mTox_cAudioFrame_free, alloc_cdata);
 }
@@ -56,6 +65,15 @@ VALUE mTox_cAudioFrame_sample_count(const VALUE self)
   return LONG2NUM(self_cdata->sample_count);
 }
 
+VALUE mTox_cAudioFrame_sample_count_ASSIGN(const VALUE self, const VALUE sample_count)
+{
+  CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
+
+  self_cdata->sample_count = NUM2LONG(sample_count);
+
+  return Qnil;
+}
+
 VALUE mTox_cAudioFrame_channels(const VALUE self)
 {
   CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
@@ -63,9 +81,27 @@ VALUE mTox_cAudioFrame_channels(const VALUE self)
   return UINT2NUM(self_cdata->channels);
 }
 
+VALUE mTox_cAudioFrame_channels_ASSIGN(const VALUE self, const VALUE channels)
+{
+  CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
+
+  self_cdata->channels = NUM2UINT(channels);
+
+  return Qnil;
+}
+
 VALUE mTox_cAudioFrame_sampling_rate(const VALUE self)
 {
   CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
 
   return ULONG2NUM(self_cdata->sampling_rate);
+}
+
+VALUE mTox_cAudioFrame_sampling_rate_ASSIGN(const VALUE self, const VALUE sampling_rate)
+{
+  CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
+
+  self_cdata->sampling_rate = NUM2ULONG(sampling_rate);
+
+  return Qnil;
 }
--- spec/lib/tox/audio_frame_spec.rb
+++ spec/lib/tox/audio_frame_spec.rb
@@ -3,6 +3,16 @@
 RSpec.describe Tox::AudioFrame do
   subject { described_class.new }
 
+  before do
+    subject.sample_count  = sample_count  if sample_count
+    subject.channels      = channels      if channels
+    subject.sampling_rate = sampling_rate if sampling_rate
+  end
+
+  let(:sample_count)  { nil }
+  let(:channels)      { nil }
+  let(:sampling_rate) { nil }
+
   describe '#initialize' do
     specify do
       expect { subject }.not_to raise_error
@@ -13,17 +23,77 @@ RSpec.describe Tox::AudioFrame do
     specify do
       expect(subject.sample_count).to be_kind_of Integer
     end
+
+    specify do
+      expect(subject.sample_count).to eq 0
+    end
+
+    context 'when it was set to positive value' do
+      let(:sample_count) { rand 0...(2**31 - 1) }
+
+      specify do
+        expect(subject.sample_count).to eq sample_count
+      end
+    end
+
+    context 'when it was set to negative value' do
+      let(:sample_count) { rand((-2**31)...0) }
+
+      specify do
+        expect(subject.sample_count).to eq sample_count
+      end
+    end
   end
 
   describe '#channels' do
     specify do
       expect(subject.channels).to be_kind_of Integer
     end
+
+    specify do
+      expect(subject.channels).to eq 0
+    end
+
+    context 'when it was set to positive value' do
+      let(:channels) { rand 0...(2**8 - 1) }
+
+      specify do
+        expect(subject.channels).to eq channels
+      end
+    end
+
+    context 'when it was set to negative value' do
+      let(:channels) { rand((-2**8 + 1)...0) }
+
+      specify do
+        expect(subject.channels).to eq 2**8 - -channels
+      end
+    end
   end
 
   describe '#sampling_rate' do
     specify do
       expect(subject.sampling_rate).to be_kind_of Integer
+    end
+
+    specify do
+      expect(subject.sampling_rate).to eq 0
+    end
+
+    context 'when it was set to positive value' do
+      let(:sampling_rate) { rand 0...(2**32 - 1) }
+
+      specify do
+        expect(subject.sampling_rate).to eq sampling_rate
+      end
+    end
+
+    context 'when it was set to negative value' do
+      let(:sampling_rate) { rand((-2**31)...0) }
+
+      specify do
+        expect(subject.sampling_rate).to eq 2**32 - -sampling_rate
+      end
     end
   end
 end
