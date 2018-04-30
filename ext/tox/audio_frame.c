610c54e7a9b3c7513d5e687955207b0721747754
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-30 03:06:11 +0000

    Add methods to Tox::AudioFrame

--- ext/tox/audio_frame.c
+++ ext/tox/audio_frame.c
@@ -4,6 +4,12 @@
 static VALUE mTox_cAudioFrame_alloc(VALUE klass);
 static void  mTox_cAudioFrame_free(mTox_cAudioFrame_CDATA *free_cdata);
 
+// Public methods
+
+static VALUE mTox_cAudioFrame_sample_count(VALUE self);
+static VALUE mTox_cAudioFrame_channels(VALUE self);
+static VALUE mTox_cAudioFrame_sampling_rate(VALUE self);
+
 /*************************************************************
  * Initialization
  *************************************************************/
@@ -12,6 +18,15 @@ void mTox_cAudioFrame_INIT()
 {
   // Memory management
   rb_define_alloc_func(mTox_cAudioFrame, mTox_cAudioFrame_alloc);
+
+  // Public methods
+
+  rb_define_method(mTox_cAudioFrame, "sample_count",
+                   mTox_cAudioFrame_sample_count, 0);
+  rb_define_method(mTox_cAudioFrame, "channels",
+                   mTox_cAudioFrame_channels, 0);
+  rb_define_method(mTox_cAudioFrame, "sampling_rate",
+                   mTox_cAudioFrame_sampling_rate, 0);
 }
 
 /*************************************************************
@@ -28,4 +43,29 @@ VALUE mTox_cAudioFrame_alloc(const VALUE klass)
 void mTox_cAudioFrame_free(mTox_cAudioFrame_CDATA *const free_cdata)
 {
   free(free_cdata);
+}
+
+/*************************************************************
+ * Public methods
+ *************************************************************/
+
+VALUE mTox_cAudioFrame_sample_count(const VALUE self)
+{
+  CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
+
+  return LONG2NUM(self_cdata->sample_count);
+}
+
+VALUE mTox_cAudioFrame_channels(const VALUE self)
+{
+  CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
+
+  return UINT2NUM(self_cdata->channels);
+}
+
+VALUE mTox_cAudioFrame_sampling_rate(const VALUE self)
+{
+  CDATA(self, mTox_cAudioFrame_CDATA, self_cdata);
+
+  return ULONG2NUM(self_cdata->sampling_rate);
 }
--- spec/lib/tox/audio_frame_spec.rb
+++ spec/lib/tox/audio_frame_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Tox::AudioFrame do
       expect { subject }.not_to raise_error
     end
   end
+
+  describe '#sample_count' do
+    specify do
+      expect(subject.sample_count).to be_kind_of Integer
+    end
+  end
+
+  describe '#channels' do
+    specify do
+      expect(subject.channels).to be_kind_of Integer
+    end
+  end
+
+  describe '#sampling_rate' do
+    specify do
+      expect(subject.sampling_rate).to be_kind_of Integer
+    end
+  end
 end
